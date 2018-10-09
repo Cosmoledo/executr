7985de1c5002c1347f3f7bf30c6b77f397353f6e
Cosmoledo <marco.fvstein@web.de>
2018-10-09 14:22:34 +0200

    Updated js

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 :: https://www.npmjs.com/package/uglify-js-es6     v. 2.8.9
 :: https://www.npmjs.com/package/uglifycss         v. 0.0.29
 
-cmd /c uglifyjs src\executr.js -o build\executr.min.js
+cmd /c uglifyjs src\executr.js -o build\executr.min.js -c
 cmd /c uglifycss src\executr.css --output build\executr.min.css
